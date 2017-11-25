4018d797c2372881e4c418ec23c85f7b1699e95f
Mika Yeap <mikaomarr@gmail.com>
2017-11-25 17:16:16 +0800

    removed old pseudo code; corrected input and output confusion

--- src/app/ml.service.ts
+++ src/app/ml.service.ts
@@ -7,13 +7,12 @@ export class MlService {
   // simulated constants
   private wheelRadius: number = (1 / (Math.PI * 2)); // ~0.16 meters
   public botWidth: number = 50 // width between wheels in px
-
+  // service properties
   private ml = require('ml-regression');
   private SLR = this.ml.SLR; // Simple Linear Regression
   private regressionModel; 
-
-  private inputCommands: number[][] = []; 
-  private outputPositionChanges: number[][] = []; 
+  private inputPositionChanges: number[][] = []; 
+  private outputCommands: number[][] = []; 
 
   constructor() { }
 
@@ -23,25 +22,15 @@ export class MlService {
     this.record(leftCmd, rightCmd, posChange.x, posChange.y);
 
     // train the model on training data
-    this.regressionModel = new this.SLR(this.inputCommands, this.outputPositionChanges); 
+    this.regressionModel = new this.SLR(this.inputPositionChanges, this.outputCommands); 
     console.log(this.regressionModel.toString(3));
-  }
-
-  /** get command as input from user */
-  getCmdInput() {
-    // get user input for random command 
-  }
-
-  /** execute given command */
-  execCmd(cmd) {
-    // move robot according to given command
   }
 
   /** record command and result of executing it as ML training data */
   record(leftCmd, rightCmd, xChange, yChange) {
     // plot left + right command, pos change X + Y as one point in ML training data
-    this.inputCommands.push([leftCmd, rightCmd]); 
-    this.outputPositionChanges.push([xChange, yChange]); 
+    this.inputPositionChanges.push([leftCmd, rightCmd]); 
+    this.outputCommands.push([xChange, yChange]); 
   }
 
   /** 
@@ -57,36 +46,7 @@ export class MlService {
    * attempt to follow path defined by an array of points
    * @param {{x, y}[]} pointsArray - Array of points that define the path to follow. 
    */
-  work(pointsArr) {
-    pointsArr.forEach((point) => {
-      this.move(point);
-      if (this.checkBattery()) {
-        this.charge();
-      }
-    }, this);
-  }
-
-  /**
-   * move bot to point specified
-   * @param {{x, y}} point - absolute (x, r) point
-   */
-  move(point) {
-    //var moveDistance = this.getPosChange(this.getPos(), point);
-    // query ML for extrapolated command
-    // move bot to point, animated, with delay to show movement
-  }
-
-  /**
-   * returns false if battery is lower than given percentage (if undefined param, default to 15%)
-   * @returns {boolean}
-   */
-  checkBattery(percentage?): boolean {
-    // check if below percentage given if not undefined
-    return true;
-  }
-
-  /** return to charging station pos */
-  charge() {
-
+  predictCommand(xDiff, yDiff) {
+    this.regressionModel.predict([[xDiff, yDiff]]); 
   }
 }
